b07c9a1f4a5378604578a806404cda1a3f2ecd30
tareqcesar-svg <tareqcesar@gmail.com>
2026-07-25 03:56:39 +0330

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
